bd43a8ed4434594359e8c93fdb86a6b66b5dd425
jlbuild <juliabuildbot@gmail.com>
2025-12-07 21:19:29 +0000

    libunistring_jll build 1.4.1+0

--- src/libunistring_jll.jl
+++ src/libunistring_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libunistring")
-JLLWrappers.@generate_main_file("libunistring", UUID("6db05002-db9d-53dd-a359-17d4854bdf22"))
+JLLWrappers.@generate_main_file("libunistring", Base.UUID("6db05002-db9d-53dd-a359-17d4854bdf22"))
 end  # module libunistring_jll
